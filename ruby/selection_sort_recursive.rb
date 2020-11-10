a76a0ccdd579afbf98d0f8b355390f509f456b8a
Rupa <rupa211984@gmail.com>
2020-11-09 17:33:30 -0800

    Add solutions

--- ruby/selection_sort_recursive.rb
+++ ruby/selection_sort_recursive.rb
@@ -1,15 +1,12 @@
-def method_name(arg)
+def selection_sort_recursive(arr)
   # type your code in here
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Expecting: 3"
-  puts method_name([2, 1, 3, 3, 2])
+  puts "Expecting: [-1, 2, 3, 5]"
+  print selection_sort_recursive([3, -1, 5, 2])
 
   puts
-
-  puts "Expecting: -1"
-  puts method_name([1, 2, 3, 4])
 
   # Don't forget to add your own!
 end
