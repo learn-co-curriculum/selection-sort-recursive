3719775bb6cf24641e3429f2c2a60b14828e13a2
Rupa <rupa211984@gmail.com>
2020-12-02 12:36:33 -0800

    Add arrows

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -5,7 +5,7 @@ function selectionSortRecursive(arr) {
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
-  console.log(selectionSortRecursive([3, -1, 5, 2]));
+  console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
   console.log("");
 }
